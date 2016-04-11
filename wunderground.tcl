c94e966ea0debe8c71ab1a6f0f7dc9e2518aafd8
thufir <thufir@tleilax.bounceme.net>
2016-04-11 04:23:59 -0700

    increment

--- wunderground.tcl
+++ wunderground.tcl
@@ -3,36 +3,53 @@ package require Expect 5.40
 package require cmdline
 
 set timeout -1
-set host rainmaker.wunderground.com
-set port 3000
-set env(TERM) vt100
 
+set env(TERM) vt100	;# actual value doesn't matter, just has to be set
 
-spawn telnet $host $port
+spawn telnet rainmaker.wunderground.com 3000
+while {1} {
+	expect timeout {
+		send_user "failed to contact weather server\n"
+		exit
+	} "Press Return to continue*" {
+               # this prompt used sometimes, eg, upon opening connection
+               send "\r"
+	} "Press Return for menu*" {
+               # this prompt used sometimes, eg, upon opening connection
+               send "\r"
+	} "M to display main menu*" {
+		# sometimes ask this if there is a weather watch in effect
+		send "M\r"
+	} "Change scrolling to screen*Selection:" {
+		break
+	} eof {
+		send_user "failed to telnet to weather server\n"
+		exit
+	}
+}
+send "C\r"
+expect timeout timedout "Selection:"
+send "4\r"
+expect timeout timedout "Selection:"
+send "1\r"
+expect timeout timedout "Selection:"
+send "1\r"
+expect timeout timedout "city code:"
+send "$code\r"
+expect $code		;# discard this
 
-
-while 1 {
-	expect "Press Return to continue:" {
+while {1} {
+	expect timeout {
+		timedout
+	} "Press Return to continue*:*" {
 		send "\r"
-	}
-
-	expect "WEATHER UNDERGROUND MAIN MENU" {
-		send "1\r"
-	}
-
-	expect "CITY FORECAST MENU" {
-		send "1\r"
-	}
-
-	expect "Press Return for menu" {
+	} "Press Return to display statement, M for menu:*" {
 		send "\r"
+	} -re "(.*)CITY FORECAST MENU.*Selection:" {
+		break
 	}
 }
 
+send "X\r"
+expect
 
-
-
-
-
-
-puts "\n\n\n************\ndone"
