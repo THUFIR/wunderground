2f0809cd1c901fd01c5f24f36f04a2d0983b9f30
thufir <thufir@tleilax.bounceme.net>
2016-04-12 03:54:40 -0700

    increment

--- client.tcl
+++ client.tcl
@@ -2,6 +2,21 @@ package require Tcl 8.4
 package require Expect 5.40
 
 
+
+
+while {1} {
+    set chan [socket 127.0.0.1 12345]         ;# Open the connection
+    puts -nonewline "Enter your name: "
+    set name [gets stdin]
+    flush stdout
+    puts $chan $name
+    flush $chan
+    close $chan
+    sleep 1
+}
+
+
+
 while {1} {
  set chan [socket 127.0.0.1 12345]         ;# Open the connection
  puts $chan hello                         ;# Send a string
