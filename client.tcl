72066b71c064950450ce160e5fb6a2e62e0a1165
thufir <thufir@tleilax.bounceme.net>
2016-04-17 04:55:14 -0700

    increment

--- client.tcl
+++ client.tcl
@@ -1,13 +1,13 @@
 package require Tcl 8.4
 package require Expect 5.40
 
-
-
 puts -nonewline "> "
 
+set host foo.com
+set port 1234
 
-set connection(host) foo.com
-set connection(port) 1234
+puts $host
+puts $port
 
 while {1} {
     set chan [socket 127.0.0.1 12345]         ;# Open the connection
