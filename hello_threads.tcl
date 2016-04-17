e6d9df8e14ff5c71390ffcb329ec04b2f8402b67
thufir <thufir@tleilax.bounceme.net>
2016-04-17 06:54:35 -0700

    increment

---
+++ hello_threads.tcl
@@ -0,0 +1,35 @@
+package require Tcl 8.4
+package require Expect 5.40
+package require Thread
+
+
+
+
+puts "*** I'm thread [thread::id]"
+
+
+
+set a [thread::create]
+set b [thread::create]
+
+
+
+for {set thread 1} {$thread <= 3} {incr thread} {
+set id [thread::create -joinable {
+# Print a hello message 3 times, waiting
+# a random amount of time between messages
+for {set i 1} {$i <= 3} {incr i} {
+after [expr { int(500*rand()) }]
+puts "Thread [thread::id] says hello"
+}
+}] ;# thread::create
+puts "*** Started thread $id"
+lappend threadIds $id
+} ;# for
+puts "*** Existing threads: [thread::names]"
+# Wait until all other threads are finished
+foreach id $threadIds {
+thread::join $id
+}
+puts "*** That's all, folks!"
+
